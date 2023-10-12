f58591e06560319d50c9a4c106b1553db1917e04
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-10-12 18:05:17 +0000

    basemodel updated to take kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -12,20 +12,33 @@ class BaseModel:
     The BaseModel class that defines all common methods
     and attributes for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         class constructor for BaseModel
+        Args:
+            args: list of arguments
+            kwargs: keyword arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         the __str__ method
         """
         return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+            self.__class__.__name__, self.id, self.__dict__
+            )
 
     def save(self):
         """
@@ -34,6 +47,11 @@ class BaseModel:
 
     def to_dict(self):
         """
+        Public instance method that returns a dictionary
+        containing all keys/values of __dict__ of the instance
+
+        Returns:
+            return the dictionary of __dict__
         """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
